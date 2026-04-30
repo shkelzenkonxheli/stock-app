65820e412ceb1d0a7f7f9b7834c1525e8d534bcb
shkelzenkonxheli <xeni_kongjeli@hotmail.com>
2026-04-30 12:11:53 +0200

    Refactor code structure for improved readability and maintainability

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,14 @@ const nextConfig: NextConfig = {
       bodySizeLimit: "5mb",
     },
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
